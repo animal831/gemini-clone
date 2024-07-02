8456413e0023430958d54a8e5d164618b94b49d0
S.Indra831 <153336265+animal831@users.noreply.github.com>
2024-07-02 12:57:07 +0100

    api adding

---
+++ src/config/Geminiapi.js
@@ -0,0 +1,32 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Replace with your actual API key if not using environment variables
+const apiKey = "AIzaSyBCWcyKT3h6NP_6J_3JNyR_e2PpvtXYqU0";
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+}
+
+export default run;
